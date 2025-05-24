d5ccde2c577668005b01e3a168a49ea2dec0afde
DEBAPRIYA MAJUMDER <guddumajumder75@gmail.com>
2025-05-24 15:00:19 +0000

    patch: updated go version in github action

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/guddu75/goblog/docs"
 	"github.com/guddu75/goblog/internal/auth"
 	"github.com/guddu75/goblog/internal/mailer"
@@ -101,16 +102,16 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// r.Use(cors.Handler(cors.Options{
-	// 	// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-	// 	AllowedOrigins: []string{"https://*", "http://*"},
-	// 	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	// 	ExposedHeaders:   []string{"Link"},
-	// 	AllowCredentials: false,
-	// 	MaxAge:           300, // Maximum value not ignored by any of major browsers
-	// }))
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	if app.config.rateLimiter.Enabled {
 		r.Use(app.RateLimiterMiddleware)
@@ -124,7 +125,6 @@ func (app *application) mount() http.Handler {
 	r.Route("/v1", func(r chi.Router) {
 		// r.With(app.BasicAuthMiddleware()).Get("/health", app.healthCheckHandler)
 		r.Get("/health", app.healthCheckHandler)
-
 		r.With(app.BasicAuthMiddleware()).Get("/debug/vars", expvar.Handler().ServeHTTP)
 
 		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
